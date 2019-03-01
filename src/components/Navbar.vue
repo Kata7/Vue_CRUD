4ca7a0ee1189020afde9a27b8b0324b6b1c37be6
Austin Moran <a.moran3257@gmail.com>
2019-03-01 11:49:07 -0700

    Navbar is almost complete, structure is good, most of the styling is complete

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,14 @@
 <template>
-  <div>
-    <p>Navbar</p>
-
+  <div class='navbar'>
+    <div class=title-container>
+      <h1 class='title'>Silly-Dilf.com</h1>
+    </div>
+    <div class='link-container'>
+      <ul class='links'>
+        <li class='button'><a href='#'>Link 1</a></li>
+        <li class='button'><a href='#'>Link 2</a></li>
+      </ul>
+    </div>
   </div> 
 </template>
 
@@ -14,6 +21,49 @@ export default {
 </script>
 
 <style scoped>
+  .navbar {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    border: 2px solid black;
+    align-content: center;
+  }
+
+  .title-container {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+
+  .title {
+    margin: 0px 0px 0px 10px;
+  }
+
+  .link-container {
+    display: flex;
+    flex-direction: row;
+    align-content: center;
+    margin: 0px 10px 0px 0px;
+  }
+
+  .links {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+    list-style: none;
+  }
+
+  .button {
+    border: 2px solid black;
+    padding: 5px 10px;
+    margin: 0px 5px;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
 
 </style>
 
